8af9fa75eeebe335f65923c6e0cf9124d860dfd0
じゅえり <supirs102@gmail.com>
2019-06-19 18:24:09 +0900

    header及びfooter固定 スマホにおける高さ調整

--- wp-content/themes/WD2 theme/footer.php
+++ wp-content/themes/WD2 theme/footer.php
@@ -1,3 +1,20 @@
-<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
+	<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
+    <script type="text/javascript">
+        // スマートフォンで全画面表示
+		$(document).ready(function(){
+		var hSize = $(window).height();
+		  $('#wrapperred').height(hSize); // アドレスバーを除いたサイズを付与
+		  $('#wrapperwhite').height(hSize);
+		  $('.mainredchild').height('calc(' + hSize + ' - 65px)');
+		  $('.mainwhitechild').height('calc(' + hSize + ' - 65px)');
+		});
+		$(window).resize(function(){ // ページをリサイズした時の処理
+		var hSize = $(window).height();
+		  $('#wrapperred').height(hSize); // アドレスバーを除いたサイズを付与
+		  $('#wrapperwhite').height(hSize);
+		  $('.mainredchild').height('calc(' + hSize +' - 65px)');
+		  $('.mainwhitechild').height('calc(' + hSize +' - 65px)');
+		});
+    </script>
 </body>
 </html>
