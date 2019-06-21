b584f6e5956dd659cdccd631f04460d43b05c990
じゅえり <supirs102@gmail.com>
2019-06-21 17:52:43 +0900

    スマホにおける高さ調整続き

--- wp-content/themes/WD2 theme/footer.php
+++ wp-content/themes/WD2 theme/footer.php
@@ -1,4 +1,20 @@
 	<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
-
+    <script type="text/javascript">
+        // スマートフォンで全画面表示
+		$(document).ready(function(){
+		var hSize = $(window).innerHeight();
+		  $('#wrapperred').innerHeight(hSize); // アドレスバーを除いたサイズを付与
+		  $('#wrapperwhite').innerHeight(hSize);
+		  $('.mainredchild').innerHeight('calc(' + hSize + ' - 65px)');
+		  $('.mainwhitechild').innerHeight('calc(' + hSize + ' - 65px - 72px)');
+		});
+		$(window).resize(function(){ // ページをリサイズした時の処理
+		var hSize = $(window).innerHeight();
+		  $('#wrapperred').innerHeight(hSize); // アドレスバーを除いたサイズを付与
+		  $('#wrapperwhite').innerHeight(hSize);
+		  $('.mainredchild').innerHeight('calc(' + hSize +' - 65px)');
+		  $('.mainwhitechild').innerHeight('calc(' + hSize +' - 65px - 72px)');
+		});
+    </script>
 </body>
 </html>
