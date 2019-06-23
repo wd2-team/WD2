5999a466e8731bff7926935108c4095c63ccbd28
じゅえり <supirs102@gmail.com>
2019-06-24 02:30:55 +0900

    ピンチ禁止metaタグ追加

--- wp-content/themes/WD2 theme/header.php
+++ wp-content/themes/WD2 theme/header.php
@@ -4,11 +4,10 @@
     <meta http-equiv="content-language" content="ja">
     <meta name="description" content="">
     <meta name="keywords" content="">
-    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,user-scalable=no">
     <title>WD2</title>
     <link rel="stylesheet" href="https://use.typekit.net/yit2jnx.css">
     <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
-
     <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/swiper.css">
     <script src="<?php echo get_template_directory_uri(); ?>/js/swiper.js"></script>
