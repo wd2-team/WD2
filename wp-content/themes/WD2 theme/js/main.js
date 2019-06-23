da20fc2a54bfa90875a282bcc662c416729fe6e8
SSM <cvwwvc102@gmail.com>
2019-06-24 01:48:27 +0900

    sticky.jsを導入　調整必須　

--- wp-content/themes/WD2 theme/js/main.js
+++ wp-content/themes/WD2 theme/js/main.js
@@ -30,7 +30,22 @@ $(function(){
     });
 });
 
+// position: stickyがブラウザで使えるかチェックするための関数
+function detectSticky() {
+  const div = document.createElement('div');
+  div.style.position = 'sticky';
+  return div.style.position.indexOf('sticky') !== -1;
+}
 
+// .stickyが指定されている要素に対してposition: stickyを適用させる関数
+function callStickyState() {
+  return new StickyState(document.querySelectorAll('.sticky'));
+}
+
+// もしブラウザでposition: stickyが使えなければ、callStickyState関数を呼び出す
+if (!detectSticky()) {
+  callStickyState();
+}
 $(function(){
     // 「.modal-open」をクリック
     $('.modal-open').click(function(){
