3d0c8bffaed2b00745ab22b41f40925a580064f4
じゅえり <supirs102@gmail.com>
2019-07-01 15:32:38 +0900

    index(red)枠組み初期設定

--- wp-content/themes/WD2 theme/index.php
+++ wp-content/themes/WD2 theme/index.php
@@ -107,7 +107,8 @@
             </div><!-- .swiper-wrapper -->
         </div><!-- .swiper-container -->
     </div><!-- #mainred -->
-        <footer>
+    <footer>
+        <div id="footerinner">
             <ul>
                 <li><a href="white-page/#slide1">ABOUT</a></li>
                 <li><a href="white-page/#slide2">SERVICE</a></li>
@@ -121,7 +122,8 @@
                 <li><a href=""><i class="fab fa-instagram"></i></a></li>
                 <li><a href=""><i class="fab fa-twitter"></i></a></li>
             </ul>
-        </footer>
-    </div><!-- #wrapperred -->
+        </div><!-- #footerinner -->
+    </footer>
+</div><!-- #wrapperred -->
 
 <?php get_footer(); ?>
--- wp-content/themes/WD2 theme/page-white.php
+++ wp-content/themes/WD2 theme/page-white.php
@@ -149,18 +149,22 @@ Template Name: white page
         </div><!-- .swiper-container -->
     </div><!-- #mainwhite -->
     <footer>
-        <ul>
-            <li><a href="#slide1">ABOUT</a></li>
-            <li><a href="#slide2">SERVICE</a></li>
-            <li><a href="#slide3">ARTICLE</a></li>
-            <li><a href="#slide4">CONTACT</a></li>
-        </ul>
-        <ul>
-            <li>© WD2 2019. All rights reserved.</li>
-        </ul>
-        <ul>
-            <li><a href=""><i class="fab fa-instagram"></i></a></li>
-            <li><a href=""><i class="fab fa-twitter"></i></a></li>
-        </ul>
+        <div id="footerinner">
+            <ul>
+                <li><a href="#slide1">ABOUT</a></li>
+                <li><a href="#slide2">SERVICE</a></li>
+                <li><a href="#slide3">ARTICLE</a></li>
+                <li><a href="#slide4">CONTACT</a></li>
+            </ul>
+            <ul>
+                <li>© WD2 2019. All rights reserved.</li>
+            </ul>
+            <ul>
+                <li><a href=""><i class="fab fa-instagram"></i></a></li>
+                <li><a href=""><i class="fab fa-twitter"></i></a></li>
+            </ul>
+        </div><!-- #footerinner -->
+    </footer>
+</div><!-- #wrapperwhite -->
 
 <?php get_footer(); ?>
