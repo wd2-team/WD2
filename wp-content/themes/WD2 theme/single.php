faa3c609f4b851321649bb62555e608b7552529b
じゅえり <supirs102@gmail.com>
2019-07-02 03:00:47 +0900

    singleheader,footer透明化 portfolioモーダルとfooterの重なり順調整

--- wp-content/themes/WD2 theme/single.php
+++ wp-content/themes/WD2 theme/single.php
@@ -1,13 +1,13 @@
 <?php get_header(); ?>
 
 <div id="wrapperwhite">
+    <header>
+        <a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/logo2.png"></a>
+    </header>
     <div id="mainwhite">
         <div class="swiper-container" id="swiperwhite">
             <div class="swiper-wrapper">
                 <div id="article" class="mainwhitechild swiper-slide no_swipe" data-hash="slide3">
-                    <header>
-                        <a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/logo2.png"></a>
-                    </header>
                     <div id="articlecontent">
                         <?php if(have_posts()): while(have_posts()):the_post(); ?>
                             <h1><?php if(has_post_thumbnail()): ?>
@@ -71,18 +71,21 @@
         </div><!-- .swiper-container -->
     </div><!-- #mainwhite -->
     <footer>
-        <ul>
-            <li><a href="white-page/#slide1">ABOUT</a></li>
-            <li><a href="white-page/#slide2">SERVICE</a></li>
-            <li><a href="white-page/#slide3">ARTICLE</a></li>
-            <li><a href="white-page/#slide4">CONTACT</a></li>
-        </ul>
-        <ul>
-            <li>© WD2 2019. All rights reserved.</li>
-        </ul>
-        <ul>
-            <li><a href=""><i class="fab fa-instagram"></i></a></li>
-            <li><a href=""><i class="fab fa-twitter"></i></a></li>
-        </ul>
+        <div id="footerinner">
+            <ul>
+                <li><a href="white-page/#slide1">ABOUT</a></li>
+                <li><a href="white-page/#slide2">SERVICE</a></li>
+                <li><a href="white-page/#slide3">ARTICLE</a></li>
+                <li><a href="white-page/#slide4">CONTACT</a></li>
+            </ul>
+            <ul>
+                <li>© WD2 2019. All rights reserved.</li>
+            </ul>
+            <ul>
+                <li><a href=""><i class="fab fa-instagram"></i></a></li>
+                <li><a href=""><i class="fab fa-twitter"></i></a></li>
+            </ul>
+        </div><!-- #footerinner -->
+    </footer>
 
 <?php get_footer(); ?>
